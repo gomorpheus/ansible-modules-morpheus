b19ef1f3f5770d05d097c7e077462e900166a859
Adam Hicks <thomas.adam.hicks@gmail.com>
2019-02-09 11:15:36 -0700

    second

--- ansible/plugins/lookup/morph_cypher.py
+++ ansible/plugins/lookup/morph_cypher.py
@@ -28,9 +28,24 @@ from ansible.module_utils.morpheus import (
 
 
 class LookupModule(LookupBase):
-    
+
     def _get_environment(self, environments, name, default_value=None):
         for env in environments:
             if name in env:
                 return env.get(name)
         return os.getenv(name, default_value)
+
+
+def run(self, terms, variables, **kwargs):
+    environments = variables.get('environment', [])
+
+
+def main(argv=sys.argv[1:]):
+    if len(argv) < 1:
+        print("Usage: hashivault.py path [key]")
+        return -1
+    print(LookupModule().run(argv, {})[0])
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
